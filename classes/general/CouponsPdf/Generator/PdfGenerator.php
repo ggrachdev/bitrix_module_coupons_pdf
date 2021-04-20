3956061e7eef512f3628d96763c2d0ead3c8394c
ggrachdev <ggrachdev@yandex.ru>
2021-04-20 14:51:27 +0400

    Include dompdf library

--- classes/general/CouponsPdf/Generator/PdfGenerator.php
+++ classes/general/CouponsPdf/Generator/PdfGenerator.php
@@ -1,5 +1,41 @@
 <?php
 
+namespace GGrach\CouponsPdf\Generator;
+
+use Dompdf\Dompdf;
+
 class PdfGenerator {
-    //put your code here
+
+    private string $codeCoupon;
+    private string $pathFolderForGenerate;
+
+    public function __construct(string $codeCoupon, string $pathFolderForGenerate) {
+        $this->codeCoupon = $codeCoupon;
+        $this->pathFolderForGenerate = $pathFolderForGenerate;
+    }
+
+    public function getCodeCoupon(): string {
+        return $this->codeCoupon;
+    }
+
+    public function getPathFolderForGenerate(): string {
+        return $this->pathFolderForGenerate;
+    }
+
+    public function setCodeCoupon(string $codeCoupon): void {
+        $this->codeCoupon = $codeCoupon;
+    }
+
+    public function setPathFolderForGenerate(string $pathFolderForGenerate): void {
+        $this->pathFolderForGenerate = $pathFolderForGenerate;
+    }
+
+    public function generate() {
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml('hello world');
+        $dompdf->setPaper('A4', 'landscape');
+        $dompdf->render();
+        $dompdf->stream();
+    }
+
 }
--- include.php
+++ include.php
@@ -1,5 +1,9 @@
 <?
+
+include_once 'classes/general/Libs/dompdf/autoload.inc.php';
+
 Bitrix\Main\Loader::registerAutoLoadClasses('ggrachdev.coupons_pdf', [
-    "\GGrach\CouponsPdf\Generator\CouponGenerator" => "classes/general/CouponsPdf/Generator/CouponGenerator.php"
+    "\GGrach\CouponsPdf\Generator\CouponGenerator" => "classes/general/CouponsPdf/Generator/CouponGenerator.php",
+    "\GGrach\CouponsPdf\Generator\PdfGenerator" => "classes/general/CouponsPdf/Generator/PdfGenerator.php",
 ]);
 ?>
